95d48d853312cfa41f0d07ac55f60aa9fa6c1e29
Rafael Batista <rafaabatistas@gmail.com>
2021-05-12 11:15:40 -0300

    Organizando os arquivos na pasta pages

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/global.scss';
+
+import { Header } from '../components/Header';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <main>
+      <Header />
+      <Component {...pageProps} />
+    </main>
+    )
+}
+
+export default MyApp
