5cbcf7e6f166dfa3eeff4c4c76527d5d50386b6c
Rafael Batista <rafaabatistas@gmail.com>
2021-05-12 14:41:55 -0300

    Criando html da area de listagem de filmes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
+import styles from '../styles/home.module.scss';
+
+export default function Home(props) {
   return (
-    <h1>Olá</h1>
+    <main className={styles.containerHome}>
+      <header>
+        <h1>Todos os filmes locados</h1>
+      </header>
+      <section className={styles.containerFilmes}>
+        <div className={styles.boxFilmes}>
+          <h2>Interestrelar </h2>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod beatae labore ipsum quae dolorum doloremque eius natus, praesentium voluptates hic error explicabo. Voluptatum aperiam tempora nisi nemo voluptas debitis consequatur! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, ex. lorem fa-rotate-27 Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis neque exercitationem eius eveniet quia ex nulla quisquam rem praesentium assumenda!</p>
+        </div>
+      </section>
+    </main>
     )
 }
