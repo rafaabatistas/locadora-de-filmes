fd9d66e4a425a3630b2218cad8e3f509bd183a6d
Rafael Batista <rafaabatistas@gmail.com>
2021-05-12 11:57:14 -0300

    Adicionando ancora aos botoes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import styles from './styles.module.scss';
 
+import Link from 'next/link';
+
 export function Header() {
     return (
         <header className={styles.containerHeader}>
             <h1>Store</h1>
             <div className={styles.boxBuntton}>
-                <button>Todos</button>
-                <button>Editar</button>
+                <button>
+                    <Link href="/"> 
+                        <a>Todos </a> 
+                    </Link>
+                </button>
+                <button>
+                    <Link href="/editar.tsx"> 
+                        <a>Editar </a> 
+                    </Link>
+                </button>
             </div>
         </header>
     )
