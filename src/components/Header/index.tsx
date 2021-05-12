a776ce7e08f16d962838f9a48f4376a1bb7a3fda
Rafael Batista <rafaabatistas@gmail.com>
2021-05-12 13:39:55 -0300

    Corrigindo area de click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,20 +4,20 @@ import Link from 'next/link';
 
 export function Header() {
     return (
-        <header className={styles.containerHeader}>
+        <nav className={styles.containerHeader}>
             <h1>Store</h1>
             <div className={styles.boxBuntton}>
-                <button>
-                    <Link href="/"> 
-                        <a>Todos </a> 
-                    </Link>
-                </button>
-                <button>
-                    <Link href="./editar"> 
-                        <a>Editar </a> 
-                    </Link>
-                </button>
+                <Link href="/"> 
+                    <a>
+                        <button>Todos </button>
+                    </a> 
+                </Link>
+                <Link href="./editar"> 
+                    <a>
+                        <button>Editar</button> 
+                    </a> 
+                </Link>
             </div>
-        </header>
+        </nav>
     )
 }
