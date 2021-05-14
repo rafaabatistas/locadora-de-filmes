fb4f738f9f5fa719e4ff4aea31af94884ed80abf
Rafael <rafaabatistas@gmail.com>
2021-05-14 11:50:13 -0300

    Criando um componente modal e configurando a funcionalidade

---
+++ src/components/ModalDetalhes/index.tsx
@@ -0,0 +1,82 @@
+import styles from './styles.module.scss';
+
+type Modal = { 
+    isOpen: boolean;
+    filme: any;
+    handleClose: any;
+}
+
+export function Modal({ filme, isOpen, handleClose }: Modal) {
+
+    return (
+        <>
+            <main className={styles.boxModalClick} 
+                style={{display: `${isOpen ? 'flex' : 'none'}`}} />
+
+            <main key={filme.titulo} className={styles.boxModal} 
+                style={{display: `${isOpen ? 'flex' : 'none'}` }}>
+
+                <div className={styles.boxButtonFechar}>
+                    <button className={styles.buttonFechar} onClick={handleClose}>
+                        <img src="./close.svg" alt="Botão de fechar" />
+                    </button>
+                </div>
+
+                <section className={styles.boxDetalhes}>
+                    <table cellSpacing={0} key={filme.titulo}>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <strong>Título:</strong>
+                                </td>
+                                <td>{filme.titulo}</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Gênero:</strong>
+                                </td>
+                                <td>{filme.genero}</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Lançamento:</strong>
+                                </td>
+                                <td>{filme.lancamento}</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Idioma:</strong>
+                                </td>
+                            <td>{filme.idioma}</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Diretor:</strong>
+                                </td>
+                                <td>{filme.diretor}</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Imagem:</strong>
+                                </td>
+                                <td>
+                                    <p>{filme.url}</p>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <strong>Sinopse:</strong> 
+                                </td>
+                                <td>
+                                    <p>{filme.sinopse}</p>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </main>
+        </>
+    )
+}
+
+
