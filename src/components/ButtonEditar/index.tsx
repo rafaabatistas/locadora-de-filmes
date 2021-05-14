33ed39588d541fb68877674cea17b3bf9b80ab64
Rafael <rafaabatistas@gmail.com>
2021-05-14 12:52:45 -0300

    Componente do button de editar os filmes

---
+++ src/components/ButtonEditar/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function ButtonEditar() {
+    return (
+        <main className={styles.buttonEdit}>
+            <img src="./edit.svg" alt="Botão de edição" />
+        </main>
+    )
+}
