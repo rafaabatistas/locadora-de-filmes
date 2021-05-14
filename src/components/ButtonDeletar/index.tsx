c729ba302039a46e206b5d3e073c673b22deeb49
Rafael <rafaabatistas@gmail.com>
2021-05-14 12:52:21 -0300

    Componente do button de deletar

---
+++ src/components/ButtonDeletar/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss';
+
+export function ButtonDeletar() {
+    return (
+        <main className={styles.buttonDelete}>
+            <img src="./delete.svg" alt="Botão de deletar" />
+        </main>
+    )
+}
