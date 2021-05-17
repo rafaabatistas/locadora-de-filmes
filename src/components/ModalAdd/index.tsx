1a38f5daa49f30d9a9a3566622a50e6dd0eba092
Rafael <rafaabatistas@gmail.com>
2021-05-17 18:50:34 -0300

    Adicionando componentes dos inputs e suas funcionalidades de adicionar filmes

--- src/components/ModalAdd/index.tsx
+++ src/components/ModalAdd/index.tsx
@@ -1,11 +1,47 @@
+import { api } from '../../services/api';
+
+import { InputText } from '../InputsTexts';
+import { InputTextArea } from '../InputsTextArea';
+
+
 import styles from './styles.module.scss';
+import { useState } from 'react';
 
 type Modal = {
     isOpen: boolean;
     handleClose: any;
+    movieList: any;
 }
 
-export function ModalAdd( { isOpen, handleClose }: Modal ) {
+export function ModalAdd( { isOpen, handleClose, movieList }: Modal ) {
+    const [addMovie, setAddMovie] = useState({
+        id: movieList.length,
+        titulo: '',
+        genero: '',
+        sinopse: '',
+        lancamento: '',
+        idioma: '',
+        diretor: '',
+        url: ''
+    })
+
+
+
+    async function AdicionarFilme() {
+        movieList.push(addMovie);
+        console.log(movieList);
+        const res = await api.post(`/locadora/`, {...addMovie})
+            .then((res) => {
+                console.log(res.status);
+                handleClose();
+                window.location.reload();
+            }).catch((err) => {
+                console.log(err)
+                handleClose();
+            })
+            console.log(res);
+    }
+
     return (
         <>
             <section className={styles.contentBackground} 
@@ -14,42 +50,60 @@ export function ModalAdd( { isOpen, handleClose }: Modal ) {
             <section className={styles.boxModal}
                 style={{display: `${isOpen ? 'flex' : 'none'}`}}>
                 <div className={styles.boxButtonFechar}>
-                    <button className={styles.buttonFechar} onClick={handleClose}>
+                    <button type="button" className={styles.buttonFechar} onClick={handleClose}>
                         <img src="./close.svg" alt="Botão de fechar" />
                     </button>
                 </div>
                 <h2>Adicionar</h2>
                 <form>
-                    <div className={styles.boxInput}>
-                        <span>Título: </span>
-                        <input type="text" name="titulo" id="titulo" />
-                    </div>
-                    <div className={styles.boxInput}>
-                        <span>Gênero: </span>
-                        <input type="text" name="genero" id="genero" />
-                    </div>
-                    <div className={styles.boxInput}>
-                        <span>Lançamento: </span>
-                        <input type="text" name="lancamento" id="lancamento" />
-                    </div>
-                    <div className={styles.boxInput}>
-                        <span>Idioma: </span>
-                        <input type="text" name="idioma" id="idioma" />
-                    </div>
-                    <div className={styles.boxInput}>
-                        <span>Diretor: </span>
-                        <input type="text" name="diretor" id="diretor" />
-                    </div>
-                    <div className={styles.boxInput}>
-                        <span>Imagem: </span>
-                        <input type="text" name="image" id="image" />
-                    </div>
-                    <div className={styles.boxInputTextArea}>
-                        <span>Sinopse: </span>
-                        <textarea id="sinopse" name="sinopse" />
-                    </div>
+                    <InputText 
+                        children={'Título'}
+                        name={'titulo'}
+                        value={addMovie.titulo}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['titulo']: e})}}
+                    />
+                    <InputText 
+                        children={'Gênero'}
+                        name={'genero'}
+                        value={addMovie.genero}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['genero']: e})}}
+                    />
+                    <InputText 
+                        children={'lancamento'}
+                        name={'lancamento'}
+                        value={addMovie.lancamento}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['lancamento']: e})}}
+                    />
+                    <InputText 
+                        children={'Idioma'}
+                        name={'idioma'}
+                        value={addMovie.idioma}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['idioma']: e})}}
+                        />
+                    <InputText 
+                        children={'Diretor'}
+                        name={'diretor'}
+                        value={addMovie.diretor}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['diretor']: e})}}
+                        />
+                    <InputText 
+                        children={'URL'}
+                        name={'url'}
+                        value={addMovie.url}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['url']: e})}}
+                        />                    
+                    <InputTextArea 
+                        children={'Sinopse'}
+                        name={'sinopse'}
+                        value={addMovie.sinopse}
+                        inputHandler={(e) => {setAddMovie({...addMovie, ['sinopse']: e})}}
+                     />
                     <div className={styles.boxButtonCriar}>
-                        <button className={styles.buttonCriar}>Adicionar</button>
+                        <button 
+                            className={styles.buttonCriar} 
+                            type="button"
+                            onClick={() => AdicionarFilme()}
+                        >Adicionar</button>
                     </div>
                 </form>
             </section>
