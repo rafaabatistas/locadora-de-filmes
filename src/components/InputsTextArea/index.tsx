5087ee11c2d40c794cfe9e8ff40125787ac29160
Rafael <rafaabatistas@gmail.com>
2021-05-17 18:19:18 -0300

    Adicionando componente do input de text-area

---
+++ src/components/InputsTextArea/index.tsx
@@ -0,0 +1,17 @@
+import styles from './styles.module.scss';
+
+type InputProps = {
+    name: string;
+    value?: string;
+    children: React.ReactNode;
+    inputHandler: (e) => void;
+}
+
+export function InputTextArea({children, name, value, inputHandler}: InputProps) {
+    return (
+        <div className={styles.boxInputTextArea}>
+            <span>{children}: </span>
+            <textarea onChange={(e) => inputHandler(e.target.value)}  id="sinopse" name={name} value={value} required />
+        </div>
+)
+}
