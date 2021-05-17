da7f39e93e1082ec31603433ca192d4109617e40
Rafael <rafaabatistas@gmail.com>
2021-05-17 19:18:34 -0300

    Enviando filmes locados para o componente do modal de adicionar

--- src/components/BoxAdicionar/index.tsx
+++ src/components/BoxAdicionar/index.tsx
@@ -3,22 +3,20 @@ import styles from './styles.module.scss';
 import { ModalAdd } from '../ModalAdd';
 import { useState } from 'react';
 
-export function BoxAdicionar() {
+export function BoxAdicionar({ list }: any) {
     const [isOpen, setIsOpen] = useState(false);
-
-    
 
     return(
         <>
             <div className={styles.boxFilmes} onClick={() => {setIsOpen(true)}}>
-                <div className={styles.boxAdicionar} >
+                <div className={styles.boxAdicionar}>
                     <div className={styles.boxIcon}>
                         <img src="./add.svg" alt="Ícone de adicionar" />
                     </div>
                 </div>
                 <h2>Adicionar Filme</h2>
             </div>
-            <ModalAdd isOpen={isOpen} handleClose={() => setIsOpen(!open)}/>
+            <ModalAdd isOpen={isOpen} handleClose={() => setIsOpen(!open)} movieList={list}/>
         </>
     )
 }
