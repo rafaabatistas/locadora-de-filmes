d0af574550aea5c7b592eb28d1fb6f5d102f9101
Rafael <rafaabatistas@gmail.com>
2021-05-19 16:38:48 -0300

    Retirando id e acrescentando função de adicionar dinamicamente o required do componente inputText

--- src/components/InputText/index.tsx
+++ src/components/InputText/index.tsx
@@ -1,17 +1,18 @@
 import styles from './styles.module.scss';
 
-type InputProps = {
+export type InputProps = {
     name: string;
     value?: string;
     children: React.ReactNode;
+    isRequired?: boolean;
     inputHandler: (e) => void;
 }
 
-export function InputText({children, name, value, inputHandler}: InputProps) {
+export function InputText({children, name, value, inputHandler, isRequired}: InputProps) {
     return (
         <div className={styles.boxInput}>
             <span>{children}: </span>
-            <input onChange={e => inputHandler(e.target.value)} type="text" name={name} id={name} value={value} required />
+            <input onChange={e => inputHandler(e.target.value)} type="text" name={name} value={value} required={isRequired} />
         </div>
     )
 }
