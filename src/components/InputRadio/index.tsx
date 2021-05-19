76f7d32f286373f36f6e230ddb3e76e53168b3a8
Rafael <rafaabatistas@gmail.com>
2021-05-19 16:34:40 -0300

    Criando componente para o inputRadio

---
+++ src/components/InputRadio/index.tsx
@@ -0,0 +1,27 @@
+import styles from './styles.module.scss';
+
+type InputProps = {
+    name: string;
+    value?: boolean;
+    isChecked: string;
+    children: React.ReactNode;
+    inputHandler: (e) => void;
+}
+
+
+export function InputRadio({children, inputHandler, isChecked, name }: InputProps) {
+    return (
+        <div className={styles.boxInput}>
+
+            <span>{children}: </span>
+
+            <input onChange={e => inputHandler(e.target.value)} type="radio" name={name} value={'true'} 
+             checked={isChecked === 'true'} />
+            <label htmlFor="Sim">Sim</label>
+
+            <input onChange={e => inputHandler(e.target.value)} type="radio" name={name} value={'false'}
+            checked={isChecked === 'false'}  />
+            <label htmlFor="Não">Não</label>
+        </div>
+    )
+}
