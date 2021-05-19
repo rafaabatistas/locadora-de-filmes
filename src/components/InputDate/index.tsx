20b7f6863c505096c77ce02bec75dba38295c6bf
Rafael <rafaabatistas@gmail.com>
2021-05-19 16:36:48 -0300

    Retirando required e id do componente inputDate

--- src/components/InputDate/index.tsx
+++ src/components/InputDate/index.tsx
@@ -11,7 +11,7 @@ export function InputDate({children, name, value, inputHandler}: InputProps) {
     return (
         <div className={styles.boxInput}>
             <span>{children}: </span>
-            <input onChange={e => inputHandler(e.target.value)} type="date" name={name} id={name} value={value} required />
+            <input onChange={e => inputHandler(e.target.value)} type="date" name={name} value={value} />
         </div>
     )
 }
