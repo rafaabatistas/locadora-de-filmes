d1400d7c7ef54f12f764a4b5e9720b3c3e46a4c8
Rafael Batista <rafaabatistas@gmail.com>
2021-05-20 14:35:57 -0300

    Indentaçao src/pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,12 @@ import '../styles/global.scss';
 import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-    return (
-        <main>
-            <Header />
-            <Component {...pageProps} />
-        </main>
-    )
+  return (
+    <main>
+      <Header />
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
-export default MyApp
+export default MyApp;
