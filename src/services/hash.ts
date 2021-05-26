06e0667389010f535db56b44989ea473dd71e995
Rafael Batista <rafaabatistas@gmail.com>
2021-05-26 15:24:02 -0300

    Funçao que cria hash para os id

---
+++ src/services/hash.ts
@@ -0,0 +1,22 @@
+export const CriarHash = () => {
+  const s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  //return 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+  return (
+    s4() +
+    s4() +
+    '-' +
+    s4() +
+    '-' +
+    s4() +
+    '-' +
+    s4() +
+    '-' +
+    s4() +
+    s4() +
+    s4()
+  );
+};
